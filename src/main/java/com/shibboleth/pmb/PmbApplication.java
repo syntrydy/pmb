9697a372a71eccac16466cd801d08f3134029184
Thomas Gasmyr <Thomas@gluu.org>
2021-11-11 23:39:27 +0100

    feat(pmb): Initial setup

---
+++ src/main/java/com/shibboleth/pmb/PmbApplication.java
@@ -0,0 +1,13 @@
+package com.shibboleth.pmb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shibboleth/pmb/PmbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shibboleth.pmb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
